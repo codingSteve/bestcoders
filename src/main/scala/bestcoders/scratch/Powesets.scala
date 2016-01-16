2a4a551b1d81c906cf7fdbead308f76ab8543f0d
Steven Powell <steven.poewll@bestcoders.co.uk>
2016-01-16 19:22:49 +0000

    .

--- src/main/scala/bestcoders/scratch/Powesets.scala
+++ src/main/scala/bestcoders/scratch/Powesets.scala
@@ -12,13 +12,10 @@ object P extends App {
 class Powersets {
   def emptySet:Set[Int] = Set()
 
-  
   def sets(args: List[Int]): Set[Set[Int]] = { 
      args match{
        case x :: xs => sets(xs).flatMap(s1 => List(s1, s1 + x))
        case nil     => Set(emptySet)
      }
   }
-  
-
 }
