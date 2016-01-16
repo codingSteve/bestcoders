9f86c26b6d5525692e5360b99de56ee214909d71
Steven Powell <steven.poewll@bestcoders.co.uk>
2016-01-16 19:20:43 +0000

    Simplify set gneration

--- src/main/scala/bestcoders/scratch/Powesets.scala
+++ src/main/scala/bestcoders/scratch/Powesets.scala
@@ -2,6 +2,7 @@
 
 object P extends App {
   def p = new Powersets()
+  
   println(p.sets(List()))
   println(p.sets(List(1)))
   println(p.sets(List(1,2)))
@@ -11,21 +12,13 @@ object P extends App {
 class Powersets {
   def emptySet:Set[Int] = Set()
 
-  def sets(args: List[Int]): Set[Set[Int]] = {
-      args match {
-        case x :: xs => {
-          val subsets = sets(xs)
-          val headOption = Set(emptySet, Set(x))
-          
-          //Cartesian product
-          for {
-            s1 <- headOption
-            s2 <- subsets
-          } yield s1 ++ s2
-        }
-        case nil     => Set(emptySet)
-      }
-      
+  
+  def sets(args: List[Int]): Set[Set[Int]] = { 
+     args match{
+       case x :: xs => sets(xs).flatMap(s1 => List(s1, s1 + x))
+       case nil     => Set(emptySet)
+     }
   }
+  
 
 }
